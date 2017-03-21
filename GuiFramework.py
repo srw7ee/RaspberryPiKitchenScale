4f15abde8521dce8922dcc1749b5cc6cfd0f6c34
Sam <srw7ee@virginia.edu>
2017-03-21 12:17:01 -0400

    framework for multiple screens from stackoverflow

--- GuiFramework.py
+++ GuiFramework.py
@@ -1,24 +1,77 @@
-import tkinter as tk
-from tkinter import ttk
-from ScaleMethods import getWeightInGrams
-from tkinter import *
+import tkinter as tk   # python3
+#import Tkinter as tk   # python
 
-MAX = 100
+TITLE_FONT = ("Helvetica", 18, "bold")
 
-root = Tk()
-root.geometry('{}x{}'.format(400, 100))
-progress_var = DoubleVar()
-theLabel = Label(root, text="Sample text to show")
-theLabel.pack()
-progressbar = ttk.Progressbar(root, variable=progress_var, maximum=MAX)
-progressbar.pack(fill=X, expand=1)
+class ScaleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (HomePage, Recipes, Recipe):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("HomePage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
-def progress_function():
-    print(getWeightInGrams())
-    progress_var.set(getWeightInGrams()/10)
-    root.update_idletasks()
-    root.after(100, progress_function)
+class HomePage(tk.Frame):
 
-progress_function()
-root.mainloop()
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Go to Page One", command=lambda: controller.show_frame("Recipe"))
+        button2 = tk.Button(self, text="Go to Page Two", command=lambda: controller.show_frame("Recipes"))
+        button1.pack()
+        button2.pack()
+
+
+class Recipes(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 1", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page", command=lambda: controller.show_frame("HomePage"))
+        button.pack()
+
+
+class Recipe(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("HomePage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    app = ScaleApp()
+    app.mainloop()
---
+++ ProgressBarDemo.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+from ScaleMethods import getWeightInGrams
+from tkinter import *
+
+MAX = 100
+
+root = Tk()
+root.geometry('{}x{}'.format(400, 100))
+progress_var = DoubleVar()
+theLabel = Label(root, text="Sample text to show")
+theLabel.pack()
+progressbar = ttk.Progressbar(root, variable=progress_var, maximum=MAX)
+progressbar.pack(fill=X, expand=1)
+
+
+def progress_function():
+    print(getWeightInGrams())
+    progress_var.set(getWeightInGrams()/10)
+    root.update_idletasks()
+    root.after(100, progress_function)
+
+progress_function()
+root.mainloop()
