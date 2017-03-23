f429dd167840844ca8c67640f4e2e387162681ba
Sam <srw7ee@virginia.edu>
2017-03-23 19:00:29 -0400

    added navigation functionality

--- GuiFramework.py
+++ GuiFramework.py
@@ -11,9 +11,16 @@ class Step():
        self.weight = weight
        self.ingredient = ingredient
 
+   def getText(self):
+       return self.text
+   def getWeight(self):
+       return self.weight
+
 TITLE_FONT = ("Helvetica", 18, "bold")
 ##RecipeList
-recipelist = [("Pancakes", "Required Ingredients: \n 1 1/2 cups of flour \n 3 1/2 teaspoons of baking powder, 1 teaspoon of salt \n 1 tablespoon of white sugar\n 1 1/4 cups of milk\n 1 egg \n 3 tablespoons of melted butter", Step("Place bowl on scale and zero",0,"N/A")),
+recipelist = [("Pancakes", "Required Ingredients: \n 1 1/2 cups of flour \n 3 1/2 teaspoons of baking powder, 1 teaspoon"
+                           " of salt \n 1 tablespoon of white sugar\n 1 1/4 cups of milk\n 1 egg \n 3 tablespoons of "
+                           "melted butter", Step("Place bowl on scale and zero",0,"N/A"), Step("Step2",0,"N/A")),
               ("Caramel", "HomePage"),
               ("Chardonnay", "HomePage"),
               ("Cheese Pizza", "HomePage"),
@@ -110,13 +117,15 @@ class Recipes(tk.Frame):
         gobutton.image=goimage
         gobutton.pack(side="right", padx=10)
 
+    ##have to have it update current page and next page with first step in recipe
     def recipeSelect(self, event):
         recipePage = self.controller.get_page("Recipe")
         w = event.widget
         index = int(w.curselection()[0])
-        value = recipelist[(index)][0]
-        recipePage.currentRecipe.set(value)
-        #print(recipePage.currentRecipe.get())
+        value = recipelist[(index)][2].getText()
+        recipePage.currentStep.set(value)
+        recipePage.recipe = index
+        #print(recipePage.currentStep.get())
         self.update_idletasks()
         #self.controller.show_frame("Recipe")
 
@@ -125,52 +134,75 @@ class Recipe(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        self.currentRecipe = tk.StringVar()
+        self.currentStep = tk.StringVar()
+        self.recipe = 0
+        self.index = 0
         ##get width and height
         global width
         global height
         ##Instruction
-        instructionlabel = tk.Label(self, textvariable = self.currentRecipe, font=TITLE_FONT)
-        instructionlabel.pack(anchor="center", fill="x", pady=height/7.5)
+        self.instructionlabel = tk.Label(self, textvariable = self.currentStep, font=TITLE_FONT,  wraplength=width*.8 )
+        self.instructionlabel.pack(anchor="w", fill="x", pady=height/5)
         ##HomeButton
-        homebutton = tk.Button(self, text="Home", command=lambda: controller.show_frame("HomePage"))
-        homeimage = ImageTk.PhotoImage(file="assets/home.png")
-        homebutton.config(image=homeimage)
-        homebutton.image = homeimage
-        homebutton.config(height=height / 10, width=width/3, bg="#3A5199")
-        homebutton.place(rely=0, relx=0, x=0, y=0, anchor='nw')
+        self.homebutton = tk.Button(self, text="Home", command=lambda: controller.show_frame("HomePage"))
+        self.homeimage = ImageTk.PhotoImage(file="assets/home.png")
+        self.homebutton.config(image=self.homeimage)
+        self.homebutton.image = self.homeimage
+        self.homebutton.config(height=height / 10, width=width/3, bg="#3A5199")
+        self.homebutton.place(rely=0, relx=0, x=0, y=0, anchor='nw')
         ##Recipesbutton
-        recipesbutton = tk.Button(self, text="Home", command=lambda: controller.show_frame("HomePage"))
-        recipesimage = ImageTk.PhotoImage(file="assets/home.png")
-        recipesbutton.config(image=recipesimage)
-        recipesbutton.image = recipesimage
-        recipesbutton.config(height=height / 10, width=width / 3, bg="#3A5199")
-        recipesbutton.place(rely=0, relx=0.5, x=0, y=0, anchor='n')
+        self.recipesbutton = tk.Button(self, text="Home", command=lambda: controller.show_frame("Recipes"))
+        self.recipesimage = tk.PhotoImage(file="assets/Book.png")
+        self.recipesimage = self.recipesimage.subsample(2, 2)
+        self.recipesbutton.config(image=self.recipesimage)
+        self.recipesbutton.image = self.recipesimage
+        self.recipesbutton.config(height=height / 10, width=width / 3, bg="#3A5199")
+        self.recipesbutton.place(rely=0, relx=0.5, x=0, y=0, anchor='n')
         ##settingsbutton
         settingsbutton = tk.Button(self, text="Home", command=lambda: controller.show_frame("HomePage"))
-        settingsimage = ImageTk.PhotoImage(file="assets/settings.png")
+        settingsimage = tk.PhotoImage(file="assets/settings.png")
+        #settingsimage = settingsimage.subsample(2, 2)
         settingsbutton.config(image=settingsimage)
         settingsbutton.image = settingsimage
         settingsbutton.config(height=height / 10, width=width / 3, bg="#3A5199")
         settingsbutton.place(rely=0, relx=1, x=0, y=0, anchor='ne')
         ##Back and Next buttons
-        nextbutton = tk.Button(self, text="Home", borderwidth=5, command=lambda: controller.show_frame("HomePage"))
-        nextimage = ImageTk.PhotoImage(file="assets/home.png")
+        nextbutton = tk.Button(self, text="Home", borderwidth=4, command=lambda: self.nextStep())
+        nextimage = tk.PhotoImage(file="assets/NextArrowWhite.png")
+        nextimage = nextimage.subsample(2, 2)
         nextbutton.config(image=nextimage)
         nextbutton.image = nextimage
         nextbutton.config(height = height/10, width=width/2, bg="#2F2E33")
         nextbutton.place(rely=1.0, relx=1.0, x=0, y=0, anchor='se')
-        backbutton = tk.Button(self, text="Home", borderwidth=5, command=lambda: controller.show_frame("HomePage"))
-        backimage = ImageTk.PhotoImage(file="assets/home.png")
-        backbutton.config(image=backimage)
-        backbutton.image = backimage
-        backbutton.config(height=height / 10, width=width / 2, bg="#2F2E33")
-        backbutton.place(rely=1.0, relx=0, x=0, y=0, anchor='sw')
+        self.backbutton = tk.Button(self, text="Home", borderwidth=4, command=lambda: self.prevStep())
+        backimage = tk.PhotoImage(file="assets/BackArrowWhite.png")
+        backimage = backimage.subsample(2, 2)
+        self.backbutton.config(image=backimage)
+        self.backbutton.image = backimage
+        self.backbutton.config(height=height / 10, width=width / 2, bg="#2F2E33")
+        self.backbutton.place(rely=1.0, relx=0, x=0, y=0, anchor='sw')
 
+    def nextStep(self):
+        if(len(recipelist[(self.recipe)]) < self.index+4):
+            self.index = 0
+            self.currentStep.set(recipelist[(self.recipe)][self.index+2].getText())
+            self.update_idletasks()
+            self.controller.show_frame("HomePage")
+        else:
+            self.index += 1
+            step = recipelist[(self.recipe)][self.index+2].getText()
+            self.currentStep.set(step)
+            # print(recipePage.currentRecipe.get())
+            self.update_idletasks()
+            # self.controller.show_frame("Recipe")
 
-
-
-
+    def prevStep(self):
+        if(self.index > 0):
+            self.index -= 1
+            step = recipelist[(self.recipe)][self.index + 2].getText()
+            self.currentStep.set(step)
+            self.update_idletasks()
+            # self.controller.show_frame("Recipe")
 
 
 if __name__ == "__main__":
